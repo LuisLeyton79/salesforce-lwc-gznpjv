c789d29b6161a1992a4e4626288edda609fd24a5
LuisLeyton79 <107451845+LuisLeyton79@users.noreply.github.com>
2022-11-14 16:41:56 -0500

    2 files modified, 2 deleted

--- src/main.js
+++ src/main.js
@@ -4,5 +4,4 @@ import Contacto from "lWC"
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
-const cont = createElement("LWC", { is: Contacto});
-document.body.appendChild(cont);
+
