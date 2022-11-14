e7a20d8416efd37ce26449a6a0f0672411e3a7b6
LuisLeyton79 <107451845+LuisLeyton79@users.noreply.github.com>
2022-11-14 16:37:39 -0500

    luis

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createElement } from "lwc";
 import App from "x/app";
+import Contacto from "lWC"
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
+const cont = createElement("LWC", { is: Contacto});
