9bd45895b11b2d110a4227bd3f95a441af6fcd3b
LuisLeyton79 <107451845+LuisLeyton79@users.noreply.github.com>
2022-11-14 16:12:18 -0500

    2 files modified, 2 added

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -4,6 +4,24 @@ export default class App extends LightningElement {
  @api name = 'Dios';
  firstName = 'Web';
  lastName = 'Component';
+ contacts = [
+  {
+      Id: '003171931112854375',
+      Name: 'Amy Taylor',
+      Title: 'COO',
+  },
+  {
+      Id: '003192301009134555',
+      Name: 'Michael Jones',
+      Title: 'CTO',
+  },
+  {
+      Id: '003848991274589432',
+      Name: 'Jennifer Wu',
+      Title: 'CEO',
+  },
+];
+
  handleInput(event){
    this.name = event.target.value;
  };
